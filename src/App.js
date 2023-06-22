d3269f5c3fa566b0e48828409792be0954375116
Ryan Silvera <ryan.silvera@icloud.com>
2023-06-21 21:49:15 -0400

    Setting up files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>CV-Creator</h1>
-      </header>
+      <Header />
+      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="Header-Container">
+            <div className="Header-Title">
+                <h1>CV Creator</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
