aa0ec2b41397ade32ab9438ead1d7817d70db9ff
Ryan Silvera <ryan.silvera@icloud.com>
2023-06-23 20:02:37 -0400

    Creating form for personal info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
+import Personal from "./components/Personal";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Personal />
     </div>
   );
 }
---
+++ src/components/Personal.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+class Personal extends Component {
+    render () {
+       return (
+        <>
+            <h3>Personal Information</h3>
+            <div className="nameField">
+                <div className="field">
+                    <label>First Name</label>
+                    <input type="text" name="firstName" className="firstname-input" placeholder="First Name"></input>
+                </div>
+                <div className="field">
+                    <label>Last Name</label>
+                    <input type="text" name="lastName" className="lastname-input" placeholder="Last Name"></input>
+                </div>
+            </div>
+            <div className="field">
+                <label>Job Title</label>
+                <input type="text" name="jobTitle" className="jobtitle-input" placeholder="Job Title"></input>
+            </div>
+            <div className="field">
+                <label>Phone Number</label>
+                <input type="text" name="phoneNumber" className="phonenumber-input" placeholder="Phone Number"></input>
+            </div>
+            <div className="field">
+                <label>Email</label>
+                <input type="text" name="email" className="email-input" placeholder="Email"></input>
+            </div>
+            <div className="field">
+                <label>Description</label>
+                <textarea type="text" name="description" cols={30} rows={5} placeholder="Describe yourself"></textarea>
+            </div>
+       </>)
+    }
+}
+
+export default Personal
